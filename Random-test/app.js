46642d18b9d342bc8cc2a37c187eea8b86f22e57
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-05-15 11:22:20 +0700

    Finish HTML Day 2

---
+++ Random-test/app.js
@@ -0,0 +1,5 @@
+let mainNav = document.getElementById('main-nav');
+let toggleNav = document.getElementById('toggle-nav');
+toggleNav.addEventListener('click', function () {
+  mainNav.classList.toggle('active')
+});
